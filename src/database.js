30a1c387934c3b13b2746e453e831d69c27d33e8
kar5788-S <karolsalvadorrodriguiez52@gmail.com>
2026-05-28 22:47:47 -0600

    Corrección de dotenv para conexión a MongoDB

--- src/database.js
+++ src/database.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
     .then((cnn) => console.log("Conectado a MongoDB."))
